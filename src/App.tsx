cf5133797a366d6d89663f32957f732a62657531
bhavna4 <bhavna@depasserinfotech.in>
2024-07-04 11:46:29 +0530

    added code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+import Todolist from "./components/Todolist"
 const App = () => {
+  
   return (
-    <div>
-      To List
-      <Button>Click me</Button>
-    </div>
+    // <div>
+    //   To List
+    //   <Button>Click me</Button>
+    // </div>
+    <>
+    <Todolist />
+    </>
   );
 };
 
